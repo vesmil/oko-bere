4546eb42c1ceb1abdb9f1447874bf08a8483324b
Milan Veselý <vesely202@gmail.com>
2022-11-18 15:30:20 +0100

    Basic TCP client-server app

--- serverApp/Program.cs
+++ serverApp/Program.cs
@@ -1,3 +1,43 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
 
-Console.WriteLine("Hello, World!");
+namespace OkoBereServer;
+
+public class Server
+{
+    private readonly TcpListener server;
+    private readonly TcpClient client;
+    private readonly NetworkStream stream;
+
+    private const int Port = 1234;
+
+    public Server()
+    {
+        server = new TcpListener(IPAddress.Any, Port);
+        
+        server.Start();
+        client = server.AcceptTcpClient();
+        stream = client.GetStream();
+        
+        var data = new byte[256];
+        
+        var read = stream.Read(data, 0, data.Length);
+        var message = Encoding.UTF8.GetString(data);
+        
+        Console.WriteLine(message);
+        
+        stream.Close();
+        client.Close();
+        server.Stop();
+    }
+}
+
+public static class Program
+{
+    public static void Main(string[] args)
+    {
+        var server = new Server();
+        
+    }
+}
